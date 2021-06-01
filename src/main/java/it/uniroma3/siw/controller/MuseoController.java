a52132a241d01c3ae24ff6a2c4971e4ac22d3fbe
Davide Sensi <davide.sen97@gmail.com>
2021-06-01 16:41:31 +0200

    1.6.1 correzione index.html

--- src/main/java/it/uniroma3/siw/controller/MuseoController.java
+++ src/main/java/it/uniroma3/siw/controller/MuseoController.java
@@ -1,11 +1,23 @@
 package it.uniroma3.siw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.service.ArtistaService;
 
 @Controller
 public class MuseoController {
+	
+	@Autowired
+	private ArtistaService artistaService;
 
-	/*@RequestMapping(value="/index"){
-		
-	}*/
+	@RequestMapping(value="/artisti" , method= RequestMethod.GET)
+	public String getArtisti(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("artista", this.artistaService.findAll());
+		return "artisti.html";
+	}
 }
