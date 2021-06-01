5fd9232d69f9d781e92d30048f00e04c5ca79d9b
Davide Sensi <davide.sen97@gmail.com>
2021-06-01 15:31:44 +0200

    1.6 aggiunta index per l'avvio su server

--- src/main/java/it/uniroma3/siw/controller/ArtistaController.java
+++ src/main/java/it/uniroma3/siw/controller/ArtistaController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,7 +34,7 @@ public class ArtistaController {
 			return "artisti.html";
 		}
 		else {
-			return "artistaFrom.html";
+			return "artistaForm.html";
 		}
 	}
 	
---
+++ src/main/java/it/uniroma3/siw/controller/MuseoController.java
@@ -0,0 +1,11 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MuseoController {
+
+	/*@RequestMapping(value="/index"){
+		
+	}*/
+}
