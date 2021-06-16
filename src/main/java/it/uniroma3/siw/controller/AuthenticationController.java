24a72155bc1a0ee766d5db07cf9aac7991190dec
Davide Sensi <davide.sen97@gmail.com>
2021-06-16 11:14:41 +0200

    1.27 correzione modifica della collezione su opera

--- src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -234,53 +234,6 @@ public class AuthenticationController {
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@RequestMapping(value = "/opera/{id}/admin/aggiungiCollezioneAOpera", method = RequestMethod.GET)
-	public String adminAggiungeCollezioneAOpera(@PathVariable("id") Long id,Model model) {
-
-		Opera opera = this.operaService.findById(id);
-		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
-		model.addAttribute("accountCorrente", credentials);
-		if (credentials.getRole().equals(Credentials.ADMIN_ROLE)) {
-			model.addAttribute("collezioni", this.collezioneService.findAll());
-			model.addAttribute("opera", opera);
-
-			return "admin/aggiungiCollezioneAOpera";
-		}
-		return "opere";
-	}
-
-	@RequestMapping(value = { "/opera/{id}/admin/aggiungiCollezioneAOpera" }, method = RequestMethod.POST)
-	public String registerCollezioneAOpera(@PathVariable("id") Long idOpera,
-				@RequestParam("collezione_id") Long idCollezione ,Model model) throws Exception{
-				Collezione collezione = this.collezioneService.findById(idCollezione);
-				Opera opera = operaService.findById(idOpera);
-				if(!collezione.getOpere().contains(opera)) {
-					collezione.addOpera(opera);
-					operaService.setCollezione(collezione, opera.getId());
-					operaService.save(opera);
-					return "redirect:/opere";
-			}
-			
-		return "admin/aggiungiCollezioneAOpera";
-		
-	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET) 
 	public String showRegisterForm (Model model) {
--- src/main/java/it/uniroma3/siw/controller/OperaController.java
+++ src/main/java/it/uniroma3/siw/controller/OperaController.java
@@ -9,13 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import it.uniroma3.siw.model.Collezione;
 import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.Opera;
+import it.uniroma3.siw.service.CollezioneService;
 import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.OperaService;
 
 @Controller
+@SessionAttributes("accountCorrente")
 public class OperaController {
 	@Autowired
 	private CredentialsService credentialsService;
@@ -23,6 +28,9 @@ public class OperaController {
 
     @Autowired
     private OperaService operaService;
+    
+    @Autowired
+	private CollezioneService collezioneService;
 
 	@RequestMapping(value = "opera/{id}/admin/removeOpera", method = RequestMethod.GET)
     public String removeOpera(@PathVariable("id") Long id, Model model) {    	
@@ -51,6 +59,38 @@ public class OperaController {
             return "redirect:/opere";
         
     }
+    
+    
+    
+    @RequestMapping(value = "/opera/{id}/admin/aggiungiCollezioneAOpera", method = RequestMethod.GET)
+	public String adminAggiungeCollezioneAOpera(@PathVariable("id") Long id,Model model) {
+
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+		model.addAttribute("accountCorrente", credentials);
+		if (credentials.getRole().equals(Credentials.ADMIN_ROLE)) {
+			model.addAttribute("collezioni", this.collezioneService.findAll());
+			model.addAttribute("opera", this.operaService.findById(id));
+
+			return "admin/aggiungiCollezioneAOpera";
+		}
+		return "opere";
+	}
+
+	@RequestMapping(value = { "/opera/{id}/admin/aggiungiCollezioneAOpera" }, method = RequestMethod.POST)
+	public String registerCollezioneAOpera(@PathVariable("id") Long idOpera,
+				@RequestParam("collezione") Collezione collezione ,Model model) throws Exception{
+				Opera opera = operaService.findById(idOpera);
+				if(!collezione.getOpere().contains(opera)) {
+					collezione.addOpera(opera);
+					operaService.setCollezione(collezione, opera.getId());
+					operaService.save(opera);
+					return "redirect:/opere";
+			}
+			
+		return "admin/aggiungiCollezioneAOpera";
+		
+	}
     
     
 }
